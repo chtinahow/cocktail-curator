39658fc00f89448cc34e05d754fdae478a786502
jjurman <jjurman@constantcontact.com>
2017-10-16 22:55:07 -0400

    about page, icons

--- client/elements/header.js
+++ client/elements/header.js
@@ -13,13 +13,32 @@ const imageStyle = `
   filter: contrast(0%) brightness(180%);
 `
 
+const titleStyle = 'grid-area: title;'
+const infoStyle = 'grid-area: info;'
+const searchStyle = 'grid-area: search;'
+
+const gridStyle = `
+  display: grid;
+  grid-template-rows: auto;
+  grid-template-columns: auto 1fr auto auto;
+  grid-column-gap: 0.25em;
+  grid-template-areas:
+    "title . info search"
+`
+
 module.exports = () => {
   return html`
-    <a href="/">
-      <h1 style=${headerStyle}>
+    <h1 style=${headerStyle} style=${gridStyle}>
+      <a href="/" style=${titleStyle}>
         Cocktail Curator
-        <img src="/drink.png" style=${imageStyle}>
-      </h1>
-    </a>
+        <img src="/drink.png" style=${imageStyle} />
+      </a>
+      <a href="/about" style=${infoStyle}>
+        <img src="/info.png" style=${imageStyle} />
+      </a>
+      <a href="/" style=${searchStyle}>
+        <img src="/search.png" style=${imageStyle} />
+      </a>
+    </h1>
   `
 }
--- client/pages/about.js
+++ client/pages/about.js
@@ -8,11 +8,44 @@ const pageStyle = `
   text-align: center;
 `
 
+const divStyle = `
+  font-size: 1.5em;
+  padding: 0.25em;
+`
+
+const nowrap = `white-space: nowrap`
+
 module.exports = () => {
   return html`
     <div>
       <header></header>
       <div style=${pageStyle}>
+        <div style=${divStyle}>
+          Cocktail Curator created by
+          <a style=${nowrap} href="https://github.com/chtinahow/">Tina Howard</a> and
+          <a style=${nowrap} href="http://jrjurman.com/">Jesse Jurman</a>
+        </div>
+        <div style=${divStyle}>
+          Project Code on
+          <a style=${nowrap} href="https://github.com/chtinahow/cocktail-curator">
+            github.com/chtinahow/cocktail-curator
+          </a>
+        </div>
+        <div style=${divStyle}>
+          <a style=${nowrap} href="http://www.thecocktaildb.com/">
+            Powered by TheCocktailDB.com
+          </a>
+        </div>
+        <div style=${divStyle}>
+          <a style=${nowrap} href="http://tram-one.io/">
+            Built using Tram-One
+          </a>
+        </div>
+        <div style=${divStyle}>
+          <a style=${nowrap} href="https://www.emojione.com/">
+            Icon from Emojione
+          </a>
+        </div>
       </div>
     </div>
   `
